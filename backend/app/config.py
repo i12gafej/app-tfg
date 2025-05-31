16c60ed887a9f1ad22d75713fb7ea22fc519bb40
i12gafej <i12gafej@uco.es>
2025-05-31 10:40:04 +0200

    Damn comma

--- backend/app/config.py
+++ backend/app/config.py
@@ -52,7 +52,7 @@ class Settings(BaseModel):
     MAIL_FROM: str = os.getenv("MAIL_FROM")
     MAIL_PORT: int = int(os.getenv("MAIL_PORT", "587"))
     MAIL_SERVER: str = os.getenv("MAIL_SERVER", "smtp.gmail.com")
-    MAIL_FROM_NAME: str = os.getenv("MAIL_FROM_NAME", "Patrimonio 2030"),
+    MAIL_FROM_NAME: str = os.getenv("MAIL_FROM_NAME", "Patrimonio 2030")
 
     FRONTEND_URL: str = os.getenv("FRONTEND_URL")
     ADMIN_EMAIL: str = os.getenv("ADMIN_EMAIL")
